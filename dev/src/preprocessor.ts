96b6a4b339bee1afd1d8036134776a7f84b508d5
deleterium <rui.santana@protonmail.com>
2021-08-18 23:40:01 -0300

    Added types and linted source

--- dev/src/preprocessor.ts
+++ dev/src/preprocessor.ts
@@ -1,87 +1,86 @@
-"use strict";
-
 // Author: Rui Deleterium
 // Project: https://github.com/deleterium/SmartC
 // License: BSD 3-Clause License
 
-//Preprocessor function. Takes source code as input and returns it preprocessed
-function preprocessor(sourcecode) {
+interface PREPROCESSOR_RULES {
+    regex: RegExp
+    type: 'DEFINE_NULL' | 'DEFINE_VAL' | 'UNDEF'
+}
 
-    const PreprocessorCodes = [
-        { regex: /^\s*#define\s+(\w+)\s*$/, type: "DEFINE_NULL" },
-        { regex: /^\s*#define\s+(\w+)\s+(.*)\s*$/, type: "DEFINE_VAL" },
-        { regex: /^\s*#undef\s+(\w+)\s*$/, type: "UNDEF" },
-    ];
-    var PreprocessorReplacements = [
-        {cname: "true", value: "1"},
-        {cname: "false", value: "0"},
-        {cname: "NULL", value: "0"},
+// Preprocessor function. Takes source code as input and returns it preprocessed
+function preprocessor (sourcecode: string) {
+    const PreprocessorCodes: PREPROCESSOR_RULES[] = [
+        { regex: /^\s*#define\s+(\w+)\s*$/, type: 'DEFINE_NULL' },
+        { regex: /^\s*#define\s+(\w+)\s+(.*)\s*$/, type: 'DEFINE_VAL' },
+        { regex: /^\s*#undef\s+(\w+)\s*$/, type: 'UNDEF' }
+    ]
+    let PreprocessorReplacements = [
+        { cname: 'true', value: '1' },
+        { cname: 'false', value: '0' },
+        { cname: 'NULL', value: '0' }
         // {cname: "SMARTC", value: ""},
-    ];
+    ]
 
-    function get_Prep_Rule(codeline) {
+    function getPrepRule (codeline: string) {
+        let parts
 
-        var parts;
-
-        for (let i=0; i < PreprocessorCodes.length; i++) {
-            parts = PreprocessorCodes[i].regex.exec(codeline);
+        for (let i = 0; i < PreprocessorCodes.length; i++) {
+            parts = PreprocessorCodes[i].regex.exec(codeline)
             if (parts !== null) {
                 return {
                     Code: PreprocessorCodes[i],
-                    parts: parts,
-                };
+                    parts: parts
+                }
             }
         }
-        return null;
+        return null
     }
 
-    function replaceDefines(codeline) {
+    function replaceDefines (codeline: string) {
+        PreprocessorReplacements.forEach(function (replacement) {
+            const rep = new RegExp('\\b' + replacement.cname + '\\b', 'g')
+            codeline = codeline.replace(rep, replacement.value)
+        })
 
-        PreprocessorReplacements.forEach( function (replacement) {
-            var rep = new RegExp("\\b"+replacement.cname+"\\b","g");
-            codeline = codeline.replace(rep, replacement.value);
-        });
-
-        return codeline;
+        return codeline
     }
 
+    const lines = sourcecode.split('\n')
+    const ret: string[] = []
 
-    var lines = sourcecode.split("\n");
-    var ret = [];
-
-    lines.forEach(function (currentLine) {
-
-        let PrepRule=get_Prep_Rule(currentLine);
-        let idx;
-
-        if (PrepRule===null) {
-            ret.push(replaceDefines(currentLine));
-            return;
+    lines.forEach(currentLine => {
+        const PrepRule = getPrepRule(currentLine)
+        let idx: number
+        if (PrepRule === null) {
+            ret.push(replaceDefines(currentLine))
+            return
         }
         switch (PrepRule.Code.type) {
-            case "DEFINE_NULL":
-                idx = PreprocessorReplacements.findIndex(obj => obj.cname == PrepRule.parts[1]);
-                if (idx == -1)
-                    PreprocessorReplacements.push({ cname: PrepRule.parts[1], value: "" });
-                else
-                    PreprocessorReplacements[idx].value = "";
-                break;
-            case "DEFINE_VAL":
-                idx = PreprocessorReplacements.findIndex(obj => obj.cname == PrepRule.parts[1]);
-                if (idx == -1)
-                    PreprocessorReplacements.push({ cname: PrepRule.parts[1], value: replaceDefines(PrepRule.parts[2]) });
-                else
-                    PreprocessorReplacements[idx].value = PrepRule.parts[2];
-                break;
-            case "UNDEF":
-                PreprocessorReplacements = PreprocessorReplacements.filter(obj => obj.cname != PrepRule.parts[1]);
-                break;
-            default:
-                //not implementd
+        case 'DEFINE_NULL':
+            idx = PreprocessorReplacements.findIndex(obj => obj.cname === PrepRule.parts[1])
+            if (idx === -1) {
+                PreprocessorReplacements.push({ cname: PrepRule.parts[1], value: '' })
+            } else {
+                PreprocessorReplacements[idx].value = ''
+            }
+            break
+        case 'DEFINE_VAL':
+            idx = PreprocessorReplacements.findIndex(obj => obj.cname === PrepRule.parts[1])
+            if (idx === -1) {
+                PreprocessorReplacements.push({ cname: PrepRule.parts[1], value: replaceDefines(PrepRule.parts[2]) })
+            } else {
+                PreprocessorReplacements[idx].value = PrepRule.parts[2]
+            }
+            break
+        case 'UNDEF':
+            PreprocessorReplacements = PreprocessorReplacements.filter(obj => obj.cname !== PrepRule.parts[1])
+            break
+        default:
+                // not implementd
         }
-        //push empty line so line numbers will not be messed
-        ret.push("");
-    });
+        // push empty line so line numbers will not be messed
+        ret.push('')
+    })
 
-    return ret.join("\n");
+    return ret.join('\n')
 }
