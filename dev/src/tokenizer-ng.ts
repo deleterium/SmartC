c512ede7626671a0d0c7a71ca796ded572a1a800
deleterium <rui.santana@protonmail.com>
2021-08-19 14:47:58 -0300

    tokenizer-ng.ts: Added types, linted source minor modifications

--- dev/src/tokenizer-ng.ts
+++ dev/src/tokenizer-ng.ts
@@ -1,5 +1,3 @@
-"use strict";
-
 // Author: Rui Deleterium
 // Project: https://github.com/deleterium/SmartC
 // License: BSD 3-Clause License
@@ -12,9 +10,16 @@
         "int", "short", "sizeof", "signed", "static",
         "switch", "typedef", "union", "unsigned"
 */
+interface PRE_TOKEN {
+    line: number
+    type: string
+    value: string
+    /** Only applicable for asm token */
+    asmText?: string
+}
 
-function tokenizer(input) {
-    const single_tokens_specs = [
+function tokenizer (input: string): PRE_TOKEN[] {
+    const singleTokensSpecs = [
         { char: '=', tokenType: 'equal' },
         { char: '*', tokenType: 'star' },
         { char: '!', tokenType: 'not' },
@@ -40,175 +45,174 @@ function tokenizer(input) {
         { char: ':', tokenType: 'colon' },
         { char: '{', tokenType: 'curly' },
         { char: '}', tokenType: 'curly' },
-        { char: '#', tokenType: 'SPECIAL' },
-    ];
+        { char: '#', tokenType: 'SPECIAL' }
+    ]
 
-    const regex_single_tokens_specs = [
-        {   //comment single line
-            start:     /^(\/\/.*)/,
-            tokenType: null,
-            addLength: 0,
+    const regexSingleTokensSpecs = [
+        { // comment single line
+            start: /^(\/\/.*)/,
+            tokenType: 'NONE',
+            addLength: 0
         },
-        {   //spaces, tabs, newlines
-            start:     /^([\s\t\n]+)/,
-            tokenType: null,
-            addLength: 0,
+        { // spaces, tabs, newlines
+            start: /^([\s\t\n]+)/,
+            tokenType: 'NONE',
+            addLength: 0
         },
-        {   //decimal numbers
-            start:     /^(\d[\d_]*\b)/,
-            tokenType: "numberDec",
-            addLength: 0,
+        { // decimal numbers
+            start: /^(\d[\d_]*\b)/,
+            tokenType: 'numberDec',
+            addLength: 0
         },
-        {   //hexadecimal numbers
-            start:     /^0[xX]([\da-fA-F][\da-fA-F_]*\b)/,
-            tokenType: "numberHex",
-            addLength: 2,
+        { // hexadecimal numbers
+            start: /^0[xX]([\da-fA-F][\da-fA-F_]*\b)/,
+            tokenType: 'numberHex',
+            addLength: 2
         },
-        {   //regular keywords
-            start:     /^(break|const|continue|do|else|exit|for|goto|halt|if|long|return|sleep|struct|void|while)/,
-            tokenType: "keyword",
-            addLength: 0,
+        { // regular keywords
+            start: /^(break|const|continue|do|else|exit|for|goto|halt|if|long|return|sleep|struct|void|while)/,
+            tokenType: 'keyword',
+            addLength: 0
         },
-        {   //exception
-            start:     /^(asm)/,
-            tokenType: "SPECIAL",
-            addLength: 0,
+        { // exception
+            start: /^(asm)/,
+            tokenType: 'SPECIAL',
+            addLength: 0
         },
-        {   //names for variables (or functions)
-            start:     /^(\w+)/,
-            tokenType: "variable",
-            addLength: 0,
-        },
-    ];
+        { // names for variables (or functions)
+            start: /^(\w+)/,
+            tokenType: 'variable',
+            addLength: 0
+        }
+    ]
 
-    const regex_double_tokens_specs = [
-        {   //multi line comments
+    const regexDoubleTokensSpecs = [
+        { // multi line comments
             start: /^\/\*/,
-            end:   /([\s\S]*?\*\/)/,
-            tokenType: null,
+            end: /([\s\S]*?\*\/)/,
+            tokenType: 'NONE',
             startLength: 2,
             removeTrailing: 0,
-            errormsg: "Missing '*/' to end comment section.",
+            errorMsg: "Missing '*/' to end comment section."
         },
-        {   //strings surrounded by double quotes
-            start: /^\"/,
-            end:   /([\s\S]*?\")/,
-            tokenType: "string",
+        { // strings surrounded by double quotes
+            start: /^"/,
+            end: /([\s\S]*?")/,
+            tokenType: 'string',
             startLength: 1,
             removeTrailing: 1,
-            errormsg: "Missing '\"' to end string.",
+            errorMsg: "Missing '\"' to end string."
         },
-        {   //strings surrounded by single quotes
-            start: /^\'/,
-            end: /([\s\S]*?\')/,
-            tokenType: "string",
+        { // strings surrounded by single quotes
+            start: /^'/,
+            end: /([\s\S]*?')/,
+            tokenType: 'string',
             startLength: 1,
             removeTrailing: 1,
-            errormsg: "Missing \"'\" to end string.",
-        },
-    ];
+            errorMsg: "Missing \"'\" to end string."
+        }
+    ]
 
-    var currChar, search, remaining_text;
-    var endParts, startParts, val, lines, found;
-    var i;
+    let currentChar:string, remainingText: string
 
-    var current = 0;
-    var tokens = [];
-    var curr_line = 1;
+    let current = 0
+    const tokens: PRE_TOKEN[] = []
+    let currentLine = 1
 
-    while(current < input.length) {
-        currChar = input.charAt(current);
-        remaining_text=input.slice(current);
+    while (current < input.length) {
+        currentChar = input.charAt(current)
+        remainingText = input.slice(current)
 
         // Resolve double regex tokens
-        found = regex_double_tokens_specs.find( function (ruleN) {
-            startParts=ruleN.start.exec(remaining_text);
+        const found = regexDoubleTokensSpecs.find(ruleN => {
+            const startParts = ruleN.start.exec(remainingText)
             if (startParts != null) {
-                endParts = ruleN.end.exec(remaining_text.slice(ruleN.startLength));
-                current += ruleN.startLength;
+                const endParts = ruleN.end.exec(remainingText.slice(ruleN.startLength))
+                current += ruleN.startLength
                 if (endParts !== null) {
-                    if (ruleN.tokenType === null) {
-                        curr_line += (endParts[1].match(/\n/g) || '').length;
-                        current+=endParts[1].length;
-                        return true;//break
+                    if (ruleN.tokenType === 'NONE') {
+                        currentLine += (endParts[1].match(/\n/g) || '').length
+                        current += endParts[1].length
+                        return true// breaks find function
                     }
-                    tokens.push({ type: ruleN.tokenType, value: endParts[1].slice(0,-ruleN.removeTrailing), line: curr_line });
-                    curr_line += (endParts[1].match(/\n/g) || '').length;
-                    current+=endParts[1].length;
-                    return true;//break
+                    tokens.push({ type: ruleN.tokenType, value: endParts[1].slice(0, -ruleN.removeTrailing), line: currentLine })
+                    currentLine += (endParts[1].match(/\n/g) || '').length
+                    current += endParts[1].length
+                    return true// breaks find function
                 }
-                throw new TypeError('At line:'+curr_line+". "+ruleN.errormsg);
+                throw new TypeError(`At line: ${currentLine}. ${ruleN.errorMsg}`)
             }
-            return false;
-        });
+            return false
+        })
         if (found !== undefined) {
-            continue;
+            // item already processed
+            continue
         }
 
         // Resolve single regex tokens
-        found = regex_single_tokens_specs.find( function (ruleN) {
-            startParts=ruleN.start.exec(remaining_text);
+        const found2 = regexSingleTokensSpecs.find(ruleN => {
+            const startParts = ruleN.start.exec(remainingText)
             if (startParts != null) {
-                if (ruleN.tokenType === null) {
-                    curr_line += (startParts[1].match(/\n/g) || '').length;
-                    current+=startParts[1].length+ruleN.addLength;
-                    return true;//break
+                if (ruleN.tokenType === 'NONE') {
+                    currentLine += (startParts[1].match(/\n/g) || '').length
+                    current += startParts[1].length + ruleN.addLength
+                    return true// breaks find function
                 }
-                if (ruleN.tokenType === "SPECIAL") {
-                    //handle asm case
-                    var asmParts;
-                    asmParts = /^(asm[^\w]*\{([\s\S]*?)\})/.exec(remaining_text);
+                if (ruleN.tokenType === 'SPECIAL') {
+                    // handle asm case
+                    const asmParts = /^(asm[^\w]*\{([\s\S]*?)\})/.exec(remainingText)
                     if (asmParts === null) {
-                        throw new TypeError('At line:'+curr_line+" Error parsing `asm { ... }` keyword");
+                        throw new TypeError('At line:' + currentLine + ' Error parsing `asm { ... }` keyword')
                     }
-                    tokens.push({ type: 'keyword', value: "asm", line: curr_line, asmText: asmParts[2] });
-                    curr_line += (asmParts[1].match(/\n/g) || '').length;
-                    current+=asmParts[1].length;
-                    return true;//break
+                    tokens.push({ type: 'keyword', value: 'asm', line: currentLine, asmText: asmParts[2] })
+                    currentLine += (asmParts[1].match(/\n/g) || '').length
+                    current += asmParts[1].length
+                    return true// breaks find function
                 }
-                tokens.push({ type: ruleN.tokenType, value: startParts[1], line: curr_line });
-                curr_line += (startParts[1].match(/\n/g) || '').length;
-                current+=startParts[1].length+ruleN.addLength;
-                return true;//break
+                tokens.push({ type: ruleN.tokenType, value: startParts[1], line: currentLine })
+                currentLine += (startParts[1].match(/\n/g) || '').length
+                current += startParts[1].length + ruleN.addLength
+                return true// breaks find function
             }
-            return false;
-        });
-        if (found !== undefined) {
-            continue;
+            return false
+        })
+        if (found2 !== undefined) {
+            continue
         }
 
         // Resolve all single tokens
-        search=single_tokens_specs.find(charDB => charDB.char === currChar);
-        if (search!==undefined) {
-            if (search.tokenType == null) {
-                current++;
-                continue;
-            } else if (search.tokenType == "SPECIAL") {
-                if (search.char == '#') {
-                    current++;
-                    lines=input.slice(current).split("\n");
-                    for (i=0, val=""; i<lines.length ; i++) {
-                        val+=lines[i];
-                        current+=lines[i].length+1;//newline!
-                        curr_line++;
-                        if (lines[i].endsWith("\\")){
-                            val=val.slice(0,-1);
-                            continue;
+        const search = singleTokensSpecs.find(charDB => charDB.char === currentChar)
+        if (search !== undefined) {
+            if (search.tokenType === 'NONE') {
+                current++
+                continue
+            } else if (search.tokenType === 'SPECIAL') {
+                if (search.char === '#') {
+                    current++
+                    const lines = input.slice(current).split('\n')
+                    let i = 0; let val = ''
+                    for (; i < lines.length; i++) {
+                        val += lines[i]
+                        current += lines[i].length + 1// newline!
+                        currentLine++
+                        if (lines[i].endsWith('\\')) {
+                            val = val.slice(0, -1)
+                            continue
                         }
-                        break;
+                        break
                     }
-                    tokens.push({ type: 'macro', value: val, line: curr_line - i - 1 });
-                    continue;
+                    tokens.push({ type: 'macro', value: val, line: currentLine - i - 1 })
+                    continue
                 }
-                throw new TypeError('At line:'+curr_line+". SPECIAL rule not implemented!");
+                throw new TypeError(`At line: ${currentLine}. SPECIAL rule not implemented in tokenizer().`)
             }
-            tokens.push({ type: search.tokenType, value: currChar, line: curr_line });
-            current++;
-            continue;
+            tokens.push({ type: search.tokenType, value: currentChar, line: currentLine })
+            current++
+            continue
         }
 
-        throw new TypeError("At line:"+curr_line+". Forbidden character: " + currChar);
+        throw new TypeError(`At line: ${currentLine}. Forbidden character found: '${currentChar}'.`)
     }
 
-    return tokens;
+    return tokens
 }
