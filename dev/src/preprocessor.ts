a45d346af17f7b51d9f880e7509f1df28131c61a
deleterium <rui.santana@protonmail.com>
2021-09-19 16:50:39 -0300

    Added ifdef, ifndef, else, endif preprocessor directives

--- dev/src/preprocessor.ts
+++ dev/src/preprocessor.ts
@@ -11,18 +11,29 @@
 function preprocess (sourcecode: string) {
     const preprocessorCodes: {
         regex: RegExp
-        type: 'DEFINE_NULL' | 'DEFINE_VAL' | 'UNDEF'
+        type: 'DEFINE_NULL' | 'DEFINE_VAL' | 'UNDEF' | 'IFDEF' | 'IFNDEF' | 'ELSE' | 'ENDIF'
     } [] = [
-        { regex: /^\s*#define\s+(\w+)\s*$/, type: 'DEFINE_NULL' },
-        { regex: /^\s*#define\s+(\w+)\s+(.*)\s*$/, type: 'DEFINE_VAL' },
-        { regex: /^\s*#undef\s+(\w+)\s*$/, type: 'UNDEF' }
+        { regex: /^\s*#\s*define\s+(\w+)\s*$/, type: 'DEFINE_NULL' },
+        { regex: /^\s*#\s*define\s+(\w+)\s+(.*)\s*$/, type: 'DEFINE_VAL' },
+        { regex: /^\s*#\s*undef\s+(\w+)\s*$/, type: 'UNDEF' },
+        { regex: /^\s*#\s*ifdef\s+(\w+)\s*$/, type: 'IFDEF' },
+        { regex: /^\s*#\s*ifndef\s+(\w+)\s*$/, type: 'IFNDEF' },
+        { regex: /^\s*#\s*else\s*$/, type: 'ELSE' },
+        { regex: /^\s*#\s*endif\s*$/, type: 'ENDIF' }
     ]
     let preprocessorReplacements = [
         { cname: 'true', value: '1' },
         { cname: 'false', value: '0' },
-        { cname: 'NULL', value: '0' }
-        // {cname: "SMARTC", value: ""},
+        { cname: 'NULL', value: '0' },
+        { cname: 'SMARTC', value: '' }
     ]
+
+    type IF_INFO = {
+        active: boolean
+        flipped: boolean
+    }
+    const ifActive: IF_INFO[] = [{ active: true, flipped: false }]
+    let currentIfLevel = 0
 
     function getPrepRule (codeline: string) {
         for (let i = 0; i < preprocessorCodes.length; i++) {
@@ -49,15 +60,22 @@ function preprocess (sourcecode: string) {
     const lines = sourcecode.split('\n')
     const ret: string[] = []
 
-    lines.forEach(currentLine => {
+    lines.forEach((currentLine, lineNo) => {
         const PrepRule = getPrepRule(currentLine)
+        const lineActive = ifActive[currentIfLevel].active
         let idx: number
         if (PrepRule === null) {
-            ret.push(replaceDefines(currentLine))
+            if (lineActive) {
+                ret.push(replaceDefines(currentLine))
+            } else {
+                // push empty line so line numbers will not be messed
+                ret.push('')
+            }
             return
         }
         switch (PrepRule.Code.type) {
         case 'DEFINE_NULL':
+            if (lineActive === false) break
             idx = preprocessorReplacements.findIndex(obj => obj.cname === PrepRule.parts[1])
             if (idx === -1) {
                 preprocessorReplacements.push({ cname: PrepRule.parts[1], value: '' })
@@ -66,6 +84,7 @@ function preprocess (sourcecode: string) {
             }
             break
         case 'DEFINE_VAL':
+            if (lineActive === false) break
             idx = preprocessorReplacements.findIndex(obj => obj.cname === PrepRule.parts[1])
             if (idx === -1) {
                 preprocessorReplacements.push({ cname: PrepRule.parts[1], value: replaceDefines(PrepRule.parts[2]) })
@@ -74,7 +93,38 @@ function preprocess (sourcecode: string) {
             }
             break
         case 'UNDEF':
+            if (lineActive === false) break
             preprocessorReplacements = preprocessorReplacements.filter(obj => obj.cname !== PrepRule.parts[1])
+            break
+        case 'IFDEF':
+            if (lineActive) currentIfLevel++
+            idx = preprocessorReplacements.findIndex(obj => obj.cname === PrepRule.parts[1])
+            if (idx !== -1) ifActive.push({ active: true, flipped: false })
+            else ifActive.push({ active: false, flipped: false })
+            break
+        case 'IFNDEF':
+            if (lineActive) currentIfLevel++
+            idx = preprocessorReplacements.findIndex(obj => obj.cname === PrepRule.parts[1])
+            if (idx === -1) ifActive.push({ active: true, flipped: false })
+            else ifActive.push({ active: false, flipped: false })
+            break
+        case 'ELSE': {
+            idx = preprocessorReplacements.findIndex(obj => obj.cname === PrepRule.parts[1])
+            const lastIfInfo = ifActive.pop()
+            if (lastIfInfo === undefined) throw new SyntaxError(`At line: ${lineNo + 1}. Unmatched '#else' directive.`)
+            if (lastIfInfo.flipped === true) throw new SyntaxError(`At line: ${lineNo + 1}. Unmatched '#else' directive.`)
+            ifActive.push({ active: !lastIfInfo.active, flipped: true })
+            if (ifActive.length === 1) {
+                throw new SyntaxError(`At line: ${lineNo + 1}. '#else' directive not associated with '#ifdef', '#ifndef' nor '#if'.`)
+            }
+            break
+        }
+        case 'ENDIF':
+            if (ifActive.length - 1 === currentIfLevel) currentIfLevel--
+            ifActive.pop()
+            if (ifActive.length === 0) {
+                throw new SyntaxError(`At line: ${lineNo + 1}. '#endif' directive not associated with '#ifdef', '#ifndef' nor '#if'.`)
+            }
             break
         default:
                 // not implementd
@@ -82,6 +132,10 @@ function preprocess (sourcecode: string) {
         // push empty line so line numbers will not be messed
         ret.push('')
     })
+
+    if (ifActive.length !== 1) {
+        throw new SyntaxError("At line: EOF. Unmatched directives '#ifdef', '#ifndef' nor '#if'.")
+    }
 
     return ret.join('\n')
 }
