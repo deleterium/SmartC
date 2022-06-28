fddf28e307201bec18dae955d77ef07aa3814be8
deleterium <rui.santana@protonmail.com>
2022-06-28 00:21:55 -0300

    Removed warning when both sides are pointers

--- src/codeGenerator/astProcessor/binaryAsnProcessor.ts
+++ src/codeGenerator/astProcessor/binaryAsnProcessor.ts
@@ -100,11 +100,15 @@ export default function binaryAsnProcessor (
         const castSide = implicitTypeCastingTest(CurrentNode.Operation.value, leftDeclaration, rightDeclaration)
         switch (castSide) {
         case 'left':
-            AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on left side of operator '${CurrentNode.Operation.value}'.`)
+            if (!(leftDeclaration.endsWith('_ptr') && rightDeclaration.endsWith('_ptr'))) {
+                AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on left side of operator '${CurrentNode.Operation.value}'.`)
+            }
             LGenObj = typeCasting(AuxVars, LGenObj, rightDeclaration, CurrentNode.Operation.line)
             break
         case 'right': {
-            AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on right side of operator '${CurrentNode.Operation.value}'.`)
+            if (!(leftDeclaration.endsWith('_ptr') && rightDeclaration.endsWith('_ptr'))) {
+                AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on right side of operator '${CurrentNode.Operation.value}'.`)
+            }
             const oldAsm = RGenObj.asmCode
             RGenObj = typeCasting(AuxVars, RGenObj, leftDeclaration, CurrentNode.Operation.line)
             // append only the diff
@@ -180,7 +184,9 @@ export default function binaryAsnProcessor (
         case 'left':
             throw new Error('Internal error')
         case 'right':
-            AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on right side of assignment '='.`)
+            if (!(lDecl.endsWith('_ptr') && rDecl.endsWith('_ptr'))) {
+                AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on right side of assignment '='.`)
+            }
             RGenObj = typeCasting(AuxVars, RGenObj, lDecl, CurrentNode.Operation.line)
         }
         // Create instruction
@@ -568,11 +574,15 @@ export default function binaryAsnProcessor (
         const castSide = implicitTypeCastingTest(CurrentNode.Operation.value, leftDeclaration, rightDeclaration)
         switch (castSide) {
         case 'left':
-            AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on left side of comparision '${CurrentNode.Operation.value}'.`)
+            if (!(leftDeclaration.endsWith('_ptr') && rightDeclaration.endsWith('_ptr'))) {
+                AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on left side of comparision '${CurrentNode.Operation.value}'.`)
+            }
             LGenObj = typeCasting(AuxVars, LGenObj, rightDeclaration, CurrentNode.Operation.line)
             break
         case 'right':
-            AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on right side of comparision '${CurrentNode.Operation.value}'.`)
+            if (!(leftDeclaration.endsWith('_ptr') && rightDeclaration.endsWith('_ptr'))) {
+                AuxVars.warnings.push(`Warning: at line ${CurrentNode.Operation.line}. Implicit type casting conversion on right side of comparision '${CurrentNode.Operation.value}'.`)
+            }
             RGenObj = typeCasting(AuxVars, RGenObj, leftDeclaration, CurrentNode.Operation.line)
         }
         assemblyCode = LGenObj.asmCode + RGenObj.asmCode
