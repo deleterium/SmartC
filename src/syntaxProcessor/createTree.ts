6b2340d6a69cbbdd692bd69d576043a13760eef2
deleterium <rui.santana@protonmail.com>
2024-02-17 12:48:03 -0300

    De-smelling code (brain overload)

--- src/syntaxProcessor/createTree.ts
+++ src/syntaxProcessor/createTree.ts
@@ -243,12 +243,10 @@ function KeywordToAST (tokens: TOKEN[], keywordLoc: number) : AST {
             Center: createTree(tokens.slice(1))
         }
     case 'register':
-        if (tokens.length === 1 || tokens[1].type !== 'Keyword') {
+        if (tokens.length === 1) {
             throw new Error(`At line: ${tokens[0].line}. Missing the variable type for 'register' use.`)
         }
-        if (tokens[1].value !== 'long' && tokens[1].value !== 'fixed' && tokens[1].value !== 'void') {
-            throw new Error(`At line: ${tokens[0].line}. 'registers' can be only types: 'long', 'fixed' or 'void'.`)
-        }
+        validateRegisterNextToken(tokens[1])
         return {
             type: 'unaryASN',
             Operation: tokens[0],
@@ -257,6 +255,16 @@ function KeywordToAST (tokens: TOKEN[], keywordLoc: number) : AST {
     default:
         // Never
         throw new Error(`Internal error at line: ${tokens[0].line}. Keyword '${tokens[0].value}' shown up.`)
+    }
+}
+
+function validateRegisterNextToken (nextToken: TOKEN) {
+    if (nextToken.type !== 'Keyword') {
+        throw new Error(`At line: ${nextToken.line}. Missing the variable type for 'register' use.`)
+    }
+    if (nextToken.value !== 'long' && nextToken.value !== 'fixed' && nextToken.value !== 'void') {
+        throw new Error(`At line: ${nextToken.line}. 'registers' can be only types: 'long', 'fixed' or 'void'. ` +
+            `Found '${nextToken.value}'.`)
     }
 }
 
